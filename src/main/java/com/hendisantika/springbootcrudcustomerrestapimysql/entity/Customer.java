04fdba7cce36ad12dc0c59358e140fefa2ff0348
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-17 13:47:53 +0700

    #4 - Add Customer Entity

---
+++ src/main/java/com/hendisantika/springbootcrudcustomerrestapimysql/entity/Customer.java
@@ -0,0 +1,40 @@
+package com.hendisantika.springbootcrudcustomerrestapimysql.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-crud-customer-rest-api-mysql
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/04/20
+ * Time: 13.41
+ */
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "name")
+    @Size(min = 3, max = 50)
+    private String name;
+
+    @Column(name = "phone")
+    @Size(min = 13, max = 30)
+    private String phone;
+}
